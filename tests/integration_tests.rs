17b19bd72a863ca56eaba8feecec86c748c3ab33
DrPlantabyte <explosivegnome@yahoo.com>
2021-09-02 11:37:03 +1000

    working on example first, then will make stubs and tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -13,5 +13,22 @@ pub fn setup() {
 fn test_feature() {
     setup();
     println!("{}", noiselib::test1());
+	
+	let seed: u64 = 1234567890; 
+	let rand_2d = noiselib::rand::LCGCoordinateRandom2D::new(seed);
+	let wrap_dimensions = [[0.0, 3.0], [0.0, 1.5]];
+	let base_frequency = 1.;
+	let frequency_scalar = 2.; // double the frequency for each octave
+	let base_magnitude = 1.;
+	let magnitude_scalar = 0.5; // halve the amplitude for each octave
+	let noise_generator = noiselib::TesselNoise2D::new(
+			&rand_2d,
+			base_frequency, 
+			frequency_scalar,
+			base_magnitude,
+			magnitude_scalar,
+			Some(wrap_dimensions)
+	);
+	
     panic!("Not implemented yet");
 }
